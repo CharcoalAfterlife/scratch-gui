d8169d58d3b29285cdbeec48a8e882c0bca4e3a6
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-05-31 15:20:13 -0700

    Create redux.js

---
+++ src/lib/themes/accent/redux.js
@@ -0,0 +1,27 @@
+const guiColors = {
+    'motion-primary': '#FFA500',
+    'motion-primary-transparent': '#FFA500e6',
+    'motion-tertiary': '#FFA500',
+
+    'looks-secondary': '#FFA500',
+    'looks-transparent': '#FFA50059',
+    'looks-light-transparent': '#FFA50026',
+    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#FFA500'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#ff4c4c',
+    checkboxActiveBorder: '#cc3333'
+};
+
+export {
+    guiColors,
+    blockColors
+};
